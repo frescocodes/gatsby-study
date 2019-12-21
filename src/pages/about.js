7fdeefc87a74d40186ab9cf413cf51c3d3d03d41
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-21 00:42:14 -0600

    about page dummy content
    -lorem ipsum text and heading

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Exercitation consequat nostrud Lorem anim sit aliquip dolore deserunt
+        incididunt mollit laborum excepteur do. Excepteur eu magna ut mollit
+        laboris labore quis ut laborum irure do laboris. Nostrud minim consequat
+        deserunt voluptate voluptate do aute reprehenderit ullamco incididunt
+        dolor Lorem.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
