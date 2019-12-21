01da1604085ec1765837b5fb54934766589644b3
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-21 00:46:22 -0600

    contact page page component
    -dummy text/content for contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { render } from "react-dom"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <a href="mailto:rapper@viper.com">rapper@viper.com</a>
+      <p>Phone: 666-420-6969</p>
+    </div>
+  )
+}
+
+export default ContactPage
