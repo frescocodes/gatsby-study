ea43357f441114689abeacbb1dbc02c737005cef
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-21 00:39:18 -0600

    file structure scaffolding
    with dummy text in blog component

---
+++ src/pages/blog.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>
+        Minim commodo consectetur dolore proident consequat eu et mollit elit
+        esse. Nulla cupidatat tempor incididunt cupidatat occaecat ad ipsum esse
+        quis irure tempor. Ipsum velit incididunt cillum pariatur aliquip
+        eiusmod fugiat aliqua veniam eu qui dolore nisi sint. Laborum magna
+        fugiat pariatur in dolore pariatur veniam. Fugiat ullamco est ad laboris
+        ad pariatur tempor id exercitation.
+      </p>
+    </div>
+  )
+}
+
+export default BlogPage
