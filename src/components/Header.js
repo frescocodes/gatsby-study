9cfca5fbf5f1345845b1f4f12e7d681383a72f36
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-22 18:19:04 -0600

    style: header component refactor and tweaks
    -styling tweaks to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,33 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { header, navLink, navList } from "./header.module.scss"
+import { header, navLink, navList, siteName } from "./header.module.scss"
 
-const links = ["About", "Contact", "Blog"]
+const links = ["Home", "About", "Contact", "Blog"]
 
 const Header = () => {
   return (
     <header className={header}>
       <nav>
-        <Link to="/">GatsbyJS Study</Link>
-        {/* {window.location.href !== "http://localhost:8000/" ? (
-          <Link to="/">Home</Link>
-        ) : (
-          <></>
-        )} */}
-
+        <h1>
+          <Link to="/" className={siteName}>
+            GatsbyJS Study
+          </Link>
+        </h1>
         <ul className={navList}>
-          {links.map(link => (
-            <li>
-              <Link to={`/${link}`.toLowerCase()} className={navLink}>
+          {links.map(link =>
+            link == "Home" ? (
+              <Link to="/" className={navLink}>
                 {link}
               </Link>
-            </li>
-          ))}
+            ) : (
+              <li>
+                <Link to={`/${link}`.toLowerCase()} className={navLink}>
+                  {link}
+                </Link>
+              </li>
+            )
+          )}
         </ul>
       </nav>
     </header>
